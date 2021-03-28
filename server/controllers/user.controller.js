4b486b064aa2bf6e4fc4e2ec67fc19a7db515adc
Armando <borelliarmando12@gmail.com>
2021-03-28 07:25:22 -0500

    User models.js

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,36 @@
+const { User } = require('../models/user.model');
+
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+module.exports.createUser = (request, response) => {
+    const { email, password } = request.body;
+    User.create({
+        firstName,
+        lastName
+    })
+        .then(person => response.json(person))
+        .catch(err => response.json(err));
+}
+module.exports.getAllUser = (request, response) => {
+    User.find({})
+        .then(persons => response.json(persons))
+        .catch(err => response.json(err))
+}
+module.exports.getUser = (request, response) => {
+    User.findOne({_id:request.params.id})
+        .then(person => response.json(person))
+        .catch(err => response.json(err))
+}
+module.exports.updateUser = (request, response) => {
+    User.findOneAndUpdate({_id: request.params.id}, request.body, {new:true})
+        .then(updatedUser => response.json(updatedUser))
+        .catch(err => response.json(err))
+}
+module.exports.deleteUser = (request, response) => {
+    User.deleteOne({ _id: request.params.id })
+        .then(deleteConfirmation => response.json(deleteConfirmation))
+        .catch(err => response.json(err))
+}
