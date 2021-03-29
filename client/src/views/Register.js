a53302ecbeeed1995c2f50b43d1fc744fe1c11c3
Armando <borelliarmando12@gmail.com>
2021-03-29 15:51:01 -0500

    s

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -3,11 +3,15 @@ import React from 'react';
 
 export default () => {
 
+  const onSubmitHandler = (event) => {
+    event.prevent.default();
+  }
+
   return (
     <div id="main-container">
       <main>
         <h2> Register </h2>
-        <form>
+        <form onSubmit={ onSubmitHandler }>
           <label for="email"> Email </label>
           <input type="email" name="email" required/>
 
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -34,3 +34,11 @@ module.exports.deleteUser = (request, response) => {
         .then(deleteConfirmation => response.json(deleteConfirmation))
         .catch(err => response.json(err))
 }
+
+// register: (req, res) => {
+//   User.create(req.body)
+//     .then(user => {
+//         res.json({ msg: "success!", user: user });
+//     })
+//     .catch(err => res.json(err));
+// }
