58acb9f765e79d7a75707523da21208ec9c9da15
Armando <borelliarmando12@gmail.com>
2021-06-22 10:56:09 -0500

    Added user model

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { User } = require('../models/user.model');
 const { Player } = require('../models/user.model');
 
 module.exports.index = (request, response) => {
@@ -35,4 +36,34 @@ module.exports.deleteAllData  = (request, response) => {
       response.status(200).send({message: "All data was cleared in database"});
     }
   });
+}
+
+module.exports.createUser = (request, response) => {
+    const { username, password } = request.body;
+    User.create({
+        username,
+        password
+    })
+        .then(user => response.json(user))
+        .catch(err => response.json(err));
+}
+module.exports.getAllUser = (request, response) => {
+    User.find({})
+        .then(user => response.json(user))
+        .catch(err => response.json(err))
+}
+module.exports.getUser = (request, response) => {
+    User.findOne({_id:request.params.id})
+        .then(user => response.json(user))
+        .catch(err => response.json(err))
+}
+module.exports.updateUser = (request, response) => {
+    User.findOneAndUpdate({_id: request.params.id}, request.body, {new:true})
+        .then(updatedUser => response.json(updatedUser))
+        .catch(err => response.json(err))
+}
+module.exports.deleteUser = (request, response) => {
+    User.deleteOne({ _id: request.params.id })
+        .then(deleteConfirmation => response.json(deleteConfirmation))
+        .catch(err => response.json(err))
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,8 +2,16 @@ const UserController = require('../controllers/user.controller');
 
 module.exports = function(app){
   app.get('/api', UserController.index);
+  
   app.post('/api/new/player', UserController.createPlayer);
   app.get('/api/players', UserController.getAllPlayers);
   app.delete('/api/player/:id/delete', UserController.deletePlayer);
+
   app.delete('/api/delete/data', UserController.deleteAllData);
+
+  app.post('/api/user/new', UserController.createUser);
+  app.get('/api/user', UserController.getAllUser);
+  app.get('/api/user/:id', UserController.getUser);
+  app.put('/api/user/:id/update', UserController.updateUser);
+  app.delete('/api/user/:id/delete', UserController.deleteUser);
 }
