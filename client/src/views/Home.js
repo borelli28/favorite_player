5eda6355020c2886d3f6480ba93a28c37f7ec43b
Armando <borelliarmando12@gmail.com>
2021-10-11 09:21:25 -0500

    Added pitchers statistics to mobile version. Home.js

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -170,25 +170,46 @@ export default props => {
             {
               (players
                 ? players.map((player, idx) => {
-                  return (
-                    <div key={idx}>
-                      <h4>{ player.playerInfo.name }</h4>
-                      <ul className="list-group">
-                        <li className="list-group-item">Team: <span>{ player.playerStats.team.name }</span></li>
-                        <li className="list-group-item">Position: <span>{ player.playerInfo.position }</span></li>
-                        <li className="list-group-item">At Bats: <span>{ player.playerStats.stat.atBats }</span></li>
-                        <li className="list-group-item">Hits: <span>{ player.playerStats.stat.hits }</span></li>
-                        <li className="list-group-item">Total Bases: <span>{ player.playerStats.stat.totalBases }</span></li>
-                        <li className="list-group-item">On Base %: <span>{ player.playerStats.stat.obp }</span></li>
-                        <li className="list-group-item">Runs Batted In: <span>{ player.playerStats.stat.rbi }</span></li>
-                        <li className="list-group-item">Strike Outs: <span>{ player.playerStats.stat.strikeOuts }</span></li>
-                        <li className="list-group-item">Runs: <span>{ player.playerStats.stat.runs }</span></li>
-                        <li className="list-group-item">Home Runs: <span>{ player.playerStats.stat.homeRuns }</span></li>
-                        <li className="list-group-item">Stolen Bases: <span>{ player.playerStats.stat.stolenBases }</span></li>
-                        <li className="list-group-item">Caught Stealing: <span>{ player.playerStats.stat.caughtStealing }</span></li>
-                      </ul>
-                    </div>
-                  )
+                  if (player.playerInfo.position !== "P") {
+                    return (
+                      <div key={idx}>
+                        <h4>{ player.playerInfo.name }</h4>
+                        <ul className="list-group">
+                          <li className="list-group-item">Team: <span>{ player.playerStats.team.name }</span></li>
+                          <li className="list-group-item">Position: <span>{ player.playerInfo.position }</span></li>
+                          <li className="list-group-item">At Bats: <span>{ player.playerStats.stat.atBats }</span></li>
+                          <li className="list-group-item">Hits: <span>{ player.playerStats.stat.hits }</span></li>
+                          <li className="list-group-item">Total Bases: <span>{ player.playerStats.stat.totalBases }</span></li>
+                          <li className="list-group-item">On Base %: <span>{ player.playerStats.stat.obp }</span></li>
+                          <li className="list-group-item">Runs Batted In: <span>{ player.playerStats.stat.rbi }</span></li>
+                          <li className="list-group-item">Strike Outs: <span>{ player.playerStats.stat.strikeOuts }</span></li>
+                          <li className="list-group-item">Runs: <span>{ player.playerStats.stat.runs }</span></li>
+                          <li className="list-group-item">Home Runs: <span>{ player.playerStats.stat.homeRuns }</span></li>
+                          <li className="list-group-item">Stolen Bases: <span>{ player.playerStats.stat.stolenBases }</span></li>
+                          <li className="list-group-item">Caught Stealing: <span>{ player.playerStats.stat.caughtStealing }</span></li>
+                        </ul>
+                      </div>
+                    )
+                  } else if (player.playerInfo.position == "P") {
+                    return (
+                      <div key={idx}>
+                        <h4>{ player.playerInfo.name }</h4>
+                        <ul className="list-group">
+                          <li className="list-group-item">Team: <span>{ player.playerStats.team.name }</span></li>
+                          <li className="list-group-item">Position: <span>{ player.playerInfo.position }</span></li>
+                          <li className="list-group-item">Games Pitched: <span>{ player.playerStats.stat.gamesPlayed }</span></li>
+                          <li className="list-group-item">Total Batters Faced: <span>{ player.playerStats.stat.battersFaced }</span></li>
+                          <li className="list-group-item">Hits: <span>{ player.playerStats.stat.hits }</span></li>
+                          <li className="list-group-item">Innings Pitched: <span>{ player.playerStats.stat.inningsPitched }</span></li>
+                          <li className="list-group-item">ERA(Earned Run Average): <span>{ player.playerStats.stat.era }</span></li>
+                          <li className="list-group-item">Total Bases Given: <span>{ player.playerStats.stat.totalBases }</span></li>
+                          <li className="list-group-item">OBP against Pitcher: <span>{ player.playerStats.stat.obp }</span></li>
+                          <li className="list-group-item">Strike Outs: <span>{ player.playerStats.stat.strikeOuts }</span></li>
+                          <li className="list-group-item">WHIP(Walks and Hits Per Inning Pitched): <span>{ player.playerStats.stat.whip }</span></li>
+                        </ul>
+                      </div>
+                    )
+                  }
                 })
               : "NO DATA"
               )
